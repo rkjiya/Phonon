12c3a04bd2bb6d34f38802a6b9dff7413383760e
Ratnesh Jiya <212590612@gehealthcare.com>
2025-02-22 11:34:12 +0530

    Initial commit

---
+++ src/main/java/com/phonon/loastesting/LoastestingApplication.java
@@ -0,0 +1,13 @@
+package com.phonon.loastesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoastestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoastestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phonon/loastesting/LoastestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phonon.loastesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoastestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
